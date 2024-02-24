0dbac1c8f8d34ef082d57371c95bb4a7eb46a51b
LeNgocBao145 <himagito@gmail.com>
2024-02-24 10:55:04 +0700

    test

--- Practice List.cpp
+++ Practice List.cpp
@@ -19,6 +19,27 @@ void ClearList(node* &lst);
 video GetListEntry(node* lst, int pos);
 void SetListEntry(node* &lst, int pos, video item);
 
+bool IsListEmpty(node* lst)
+{
+
+}
+
+int GetListLength(node* lst)
+{
+
+}
+
+int AddItemToList(node* &lst, video item)
+{
+
+}
+
+int InsertToList(node* &lst, int newpos, video item);
+int RemoveFromList(node* &lst, int pos);
+void ClearList(node* &lst);
+video GetListEntry(node* lst, int pos);
+void SetListEntry(node* &lst, int pos, video item);
+
 int main()
 {
 
