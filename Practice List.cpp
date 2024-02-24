5d80668ecb40391c9a3ec7ce18f18f4980ab37eb
LeNgocBao145 <himagito@gmail.com>
2024-02-24 10:56:12 +0700

    update

--- Practice List.cpp
+++ Practice List.cpp
@@ -34,11 +34,30 @@ int AddItemToList(node* &lst, video item)
 
 }
 
-int InsertToList(node* &lst, int newpos, video item);
-int RemoveFromList(node* &lst, int pos);
-void ClearList(node* &lst);
-video GetListEntry(node* lst, int pos);
-void SetListEntry(node* &lst, int pos, video item);
+int InsertToList(node* &lst, int newpos, video item)
+{
+
+}
+
+int RemoveFromList(node* &lst, int pos)
+{
+
+}
+
+void ClearList(node* &lst)
+{
+
+}
+
+video GetListEntry(node* lst, int pos)
+{
+
+}
+
+void SetListEntry(node* &lst, int pos, video item)
+{
+
+}
 
 int main()
 {
