10a7d9a091b1e9b07b07bb7b3aa8e1b26fb8b47c
LeNgocBao145 <himagito@gmail.com>
2024-02-24 10:44:17 +0700

    add file

---
+++ Practice Linked List.cpp
@@ -0,0 +1,9 @@
+
+bool IsListEmpty(node* lst);
+int GetListLength(node* lst);
+int AddItemToList(node* &lst, video item);
+int InsertToList(node* &lst, int newpos, video item);
+int RemoveFromList(node* &lst, int pos);
+void ClearList(node* &lst);
+video GetListEntry(node* lst, int pos);
+void SetListEntry(node* &lst, int pos, video item);
